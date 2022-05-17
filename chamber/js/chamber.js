7c16122f88c6edab28a0d62b64feeeae5216e823
mackeitor <nav22003@byui.edu>
2022-05-17 12:58:31 -0500

    finishing footer

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,19 +1,9 @@
-// select the elements to manipulate (output to)
-/*const datefield = document.querySelector(".date");*/
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
-
-// derive the current date using a date object
+const dateField = document.getElementById('date');
 const now = new Date();
-/*const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now);*/
-
-    const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+const formattedNow = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(now);
-// long, medium, short options ... try them
-
-/*datefield.innerHTML = `<em>${fulldate}</em>`;*/
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+dateField.innerHTML = formattedNow;
 
 
 document.querySelector(
