2e811e2afd3455b34f1df6d01e28b44b8ce05518
mackeitor <nav22003@byui.edu>
2022-05-20 19:01:20 -0500

    dom manip

---
+++ week05/js/bom.js
@@ -0,0 +1,25 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  const chapter = input.value;
+  input.value = '';  
+
+  const chapterName = document.createElement('li');
+  const chapterList = document.createElement('span');
+  const deleteButton = document.createElement('button');
+
+  chapterName.appendChild(chapterList);
+  chapterList.textContent = chapter;
+  chapterName.appendChild(deleteButton);
+  deleteButton.textContent = 'X';
+  list.appendChild(chapterName);
+
+  deleteButton.addEventListener('click', () => {
+    list.removeChild(chapterName);
+  });
+
+  
+});
+
